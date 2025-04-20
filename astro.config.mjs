cc9960843fabd5fb79e4684a1fcf6c9bb0fe8aa8
Summer_mint3 <Summer_mint3@noreply.gitcode.com>
2025-04-20 15:02:34 +0800

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://summerzhouzihan.github.io/",
+  site: USER_SITE,
   output: "static",
   style: {
     scss: {
