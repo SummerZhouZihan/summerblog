7aeb614511aefe2fbf822954b54a805c85fe1326
Summer_mint3 <Summer_mint3@noreply.gitcode.com>
2025-04-20 14:50:22 +0800

    完成页面设置,设置github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: USER_SITE,
+  site: "https://summerzhouzihan.github.io/",
   output: "static",
   style: {
     scss: {
